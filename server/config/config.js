749bd3f6609f8c4b79a9908ecd09cef1cedecfb0
andres.barros <andres.barros@signeblock.com>
2022-03-30 16:02:21 +0200

    enviroment variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
 // config.js
-// const MONGO_USERNAME = process.env.MONGO_USERNAME;
-// const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
+
 const uriConnection = {
   dbUrl:
-      'mongodb+srv://admin:1234@cluster0.v4zvi.mongodb.net/Cluster0?retryWrites=true&w=majority'
+      `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.v4zvi.mongodb.net/${MONGO_HOSTNAME}?retryWrites=true&w=majority`
 };
 
 module.exports = uriConnection;
